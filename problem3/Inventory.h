df45ff35eb3db88159aec089ed1bb784a0ad08c3
PDY <pdy127001@gmail.com>
2025-08-26 20:14:38 +0900

    도전 기능 추가(복사 생성자 및 대입함수 구현, addItem기능 확장, capacity늘려주는 함수구현, 아이템을 가격 오름차순으로 정렬하는 함수 구현)

--- problem3/Inventory.h
+++ problem3/Inventory.h
@@ -17,6 +17,15 @@ public:
 		capacity_ = capacity;
 		pItems_ = new T[capacity_];
 	}
+	Inventory(const Inventory<T>& other) {
+		capacity_ = other.capacity_;
+		size_ = other.size_;
+		pItems_ = new T[capacity_];
+		for (int i = 0; i < size_; ++i) {
+			pItems_[i] = other.pItems_[i];
+		}
+		cout << "인벤토리 복사 완료" << endl;
+	}
 	~Inventory<T>() {
 		delete[] pItems_;
 		pItems_ = nullptr;
@@ -26,6 +35,21 @@ public:
 			pItems_[size_] = item;
 			size_++;
 		}
+		else {
+			Resize(capacity_);
+			T* newItems = new T[capacity_];
+			for (int i = 0; i < size_; i++) {
+				newItems[i] = pItems_[i];
+			}
+			newItems[size_] = item;
+			size_++;
+			delete[] pItems_;
+			pItems_ = newItems;
+		}
+
+	}
+	void Resize(int newCapacity) {
+		capacity_ =newCapacity*2;
 	}
 	void RemoveLastItem() {
 		if (size_ == 0) {
@@ -42,5 +66,29 @@ public:
 			pItems_[i].PrintInfo();
 		}
 		return;
+	}
+    void Assign(const Inventory<T>& other) {
+		if (this == &other) {
+			return;
+		}
+		delete[] pItems_;
+		capacity_ = other.capacity_;
+		size_ = other.size_;
+		pItems_ = new T[capacity_];
+		for (int i = 0; i < size_; ++i) {
+			pItems_[i] = other.pItems_[i];
+		}
+		cout << "인벤토리 대입 완료" << endl;
+	}
+	void SortItems() {
+		for (int i = 0; i < size_; i++) {
+			for(int j=0;j<size_-1-i;j++){
+				if(pItems_[j].GetPrice()>pItems_[j+1].GetPrice()){
+					T temp=pItems_[j];
+					pItems_[j]=pItems_[j+1];
+					pItems_[j+1]=temp;
+				}
+			}
+		}
 	}
 };
